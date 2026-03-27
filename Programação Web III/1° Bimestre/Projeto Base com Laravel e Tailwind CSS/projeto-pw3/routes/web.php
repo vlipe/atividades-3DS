9b488b19eae2c586fdbe495c419a5a1c4a53b18c
vlipe <felipevivenciorodrigues@gmail.com>
2026-03-27 00:50:06 -0300

    finalização do projeto v.0. - laravel bootcamp.

--- Programação Web III/1° Bimestre/Projeto Base com Laravel e Tailwind CSS/projeto-pw3/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () { return view('welcome'); });
-Route::get('/login', function () { return view('login'); });
-Route::get('/cadastro', function () { return view('cadastro'); });
